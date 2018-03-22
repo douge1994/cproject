c2cfaec685a87bae626662ffe5ce036f6b4df191
wangchuan <991984903@qq.com>
2018-03-22 16:39:54 +0800

    'new'

--- webpack/app/runnoob1.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by 说个锤子 on 2017/6/26.
- */
-//document.write("it works");
-//document.write(require("./runoob2.js"))
-require('!style-loader!css-loader!./style.css');
-document.write(require('./runoob2.js'))
--- webpack/app/runoob2.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by 说个锤子 on 2017/6/26.
- */
-module.exports = 'it works from runoob2.js';
